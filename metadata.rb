99df3267aa4a24b2cd284d3e9a9278af60330266
Seth Vargo <sethvargo@gmail.com>
2012-07-08 17:03:52 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'webops@customink.com'
 license          'Apache 2.0'
 description      'Installs/Configures phantomjs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 recipe 'phantomjs::default', 'Install phantomjs'
 
